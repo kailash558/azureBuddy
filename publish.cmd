f3eb75b86fef5dff67ee1b862e6380b7ce73325e
Kailash Kumawat <31000726+kailash558@users.noreply.github.com>
2019-06-19 11:05:32 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=azurebuddy-Web-Deploy.pubxml -p:Password=b3oJexeTuKpLoHqh4b0piqPMe2ubfGH4ivjhfWrlZglvXlkbWK49wsnlH1u0
+
